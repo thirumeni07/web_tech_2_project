761421b98dbccac74146f6c779e1b87b0c6453f9
thirumeni07 <c0788609@mylambton.ca>
2021-03-05 01:51:49 +0530

    Added jquery code for fligt search. Will add more code to it.

--- js/home.js
+++ js/home.js
@@ -19,4 +19,31 @@ $('.collapse-btn').click(function(){
 //    $('.collapse-cls, closest_cls').toggle();
 })
 
+$("#development_in_progress").click(function(){
+    $.ajaxSetup({
+        headers : {
+                    "x-rapidapi-key": "6b1dd19125msh66c428d656f8b00p14948djsn7b53e22d2979",
+                    "x-rapidapi-host": "skyscanner-skyscanner-flight-search-v1.p.rapidapi.com"
+        }
+    });
+
+$.getJSON("https://skyscanner-skyscanner-flight-search-v1.p.rapidapi.com/apiservices/browsequotes/v1.0/US/USD/en-US/SFO-sky/JFK-sky/2021-03-21", function(response){
+    var count = 0;
+    $.each(response,function(key,value){	  	
+        count += 1;
+        var child_count = 0;
+        console.log("Count is "+count);
+        console.log(value);
+        $.each(value,function(k,v){
+            child_count += 1;
+            console.log("Child Count is "+child_count);
+            if('OutboundLeg' in v){
+                console.log(v.OutboundLeg.DepartureDate.substring(11,19));	
+            }	  		
+            //console.log(v.OutboundLeg['DepartureDate']);
+        })
+  })
+});       
+});
+
 });
