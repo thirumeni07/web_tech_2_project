bdd564b8aff3ef5079d9ca4bf1fa1515ae996d45
thirumeni07 <c0788609@mylambton.ca>
2021-03-05 06:04:09 +0530

    Jquery changes to display api data on html table

--- js/home.js
+++ js/home.js
@@ -4,8 +4,20 @@ $('#search-button').click(function(){
 
     if($('#fromplace').val() == '' || $('#dest').val() == '' )
         alert('Please enter the required fields')
-    else
+    else{
         $('.search-results').show();
+        $.ajaxSetup({
+            headers : {
+                        "x-rapidapi-key": "6b1dd19125msh66c428d656f8b00p14948djsn7b53e22d2979",
+                        "x-rapidapi-host": "skyscanner-skyscanner-flight-search-v1.p.rapidapi.com"
+            }
+        });
+    
+    $.getJSON("https://skyscanner-skyscanner-flight-search-v1.p.rapidapi.com/apiservices/browsequotes/v1.0/US/USD/en-US/SFO-sky/JFK-sky/2021-03-21", function(response){
+        $(".Departure").html(response.Quotes[0].OutboundLeg.DepartureDate.substring(11,16) + " AM");
+        $(".price").html("$ "+response.Quotes[0].MinPrice);
+    });  
+    }        
 });
 
 $('.clear_button').click(function(){
@@ -28,21 +40,7 @@ $("#development_in_progress").click(function(){
     });
 
 $.getJSON("https://skyscanner-skyscanner-flight-search-v1.p.rapidapi.com/apiservices/browsequotes/v1.0/US/USD/en-US/SFO-sky/JFK-sky/2021-03-21", function(response){
-    var count = 0;
-    $.each(response,function(key,value){	  	
-        count += 1;
-        var child_count = 0;
-        console.log("Count is "+count);
-        console.log(value);
-        $.each(value,function(k,v){
-            child_count += 1;
-            console.log("Child Count is "+child_count);
-            if('OutboundLeg' in v){
-                console.log(v.OutboundLeg.DepartureDate.substring(11,19));	
-            }	  		
-            //console.log(v.OutboundLeg['DepartureDate']);
-        })
-  })
+    
 });       
 });
 
