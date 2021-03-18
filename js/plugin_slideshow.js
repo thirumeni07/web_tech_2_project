fc4a989accc1180fe908c7f44ebe041ad0c44251
amandeep212 <amandeep1992kaur10@gmail.com>
2021-03-18 13:17:11 -0400

    plugin slide show in index.html

---
+++ js/plugin_slideshow.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+
+
+    $('#imageSliderAuto').bxSlider({
+        auto: true,
+        autoControls: true,
+        stopAutoOnClick: true,
+        pager: true,
+        slideWidth: 1600
+    });
+});
