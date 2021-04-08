9b69cb12a71f35856d2516c60b750e76e547209b
Rajandeepraj <78892952+Rajandeepraj@users.noreply.github.com>
2021-04-08 16:08:55 +0530

    Check login credentials

--- js/login.js
+++ js/login.js
@@ -1,7 +1,24 @@
-$(document).ready(function(){
+//$(document).ready(function(){
     
-    $('#submit').click(function(){
+  //  $('#submit').click(function(){
         
-        var currentUrl = $(location).attr('href','../index.html');
-    })
-})
+    //    var currentUrl = $(location).attr('href','../index.html');
+//    })
+//  })
+ $(function(){
+        
+        $('#main-form').validate({
+            rules:{
+                GetEmail:{  
+                      required:true,
+                      email:true
+                         },
+                GetPassword:  {required: true, minlength: 5},
+            },
+            messages:{
+                required: '<br>',
+                GetEmail: '<br> Please enter valid Email',
+                GetPassword: 'Please enter valid Password',
+            }}
+        )
+    });
